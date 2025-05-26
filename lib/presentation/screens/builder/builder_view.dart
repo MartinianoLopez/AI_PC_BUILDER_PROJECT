d4f7cf9b66f0b350b4add79b8ad45e5dc7bfb98d
naranjaFanta <eduardo.tecmet@hotmail.com>
2025-05-26 19:09:57 -0300

    modificacion en home en solicitudDeIngresoAlArmador y boton editar . ademas de en componentsview , en botn guardar, para quqe al finalizar un armado o editar se actualice al instante en home ( sin necesidad de actualizar la pagina manualmente )

--- lib/presentation/screens/builder/builder_view.dart
+++ lib/presentation/screens/builder/builder_view.dart
@@ -430,6 +430,8 @@ class _RouteButtons extends StatelessWidget {
                           ),
                         ),
                       );
+                      Navigator.pop(context, true); // <- Esta línea hace que HomeScreen sepa que debe recargar
+
                     }
                   } catch (e) {
                     if (context.mounted) {
